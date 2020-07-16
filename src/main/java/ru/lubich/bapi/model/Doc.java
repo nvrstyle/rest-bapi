f9b9ab388d09bb432de8df2f9786771e15969bea
nvrstyle <nvrstyle@gmail.com>
2020-07-17 00:53:27 +0300

    Added Doc entity

---
+++ src/main/java/ru/lubich/bapi/model/Doc.java
@@ -0,0 +1,83 @@
+package ru.lubich.bapi.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Документ сотрудника
+ */
+@Entity
+@Table(name = "Doc")
+public class Doc {
+
+    /**
+     * Идентификатор
+     */
+    @Id
+    @Column(name = "user_id")
+    private Long id;
+
+    /**
+     * Номер документа
+     */
+    @Column(name = "number", length = 50, nullable = false)
+    private String number;
+
+    /**
+     * Дата документа
+     */
+    @Temporal(TemporalType.DATE)
+    @Column(name = "date", nullable = false)
+    private Date date;
+
+    @Version
+    private Integer version = 0;
+
+    /**
+     * Вид документа
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "type_id", nullable = false)
+    private DocType docType;
+
+    /**
+     * Сотрудник, которому принадлежит документ
+     */
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private User user;
+
+    /**
+     * Конструктор для Hibernate
+     */
+    public Doc() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public DocType getDocType() {
+        return docType;
+    }
+
+    public void setDocType(DocType docType) {
+        this.docType = docType;
+    }
+}
