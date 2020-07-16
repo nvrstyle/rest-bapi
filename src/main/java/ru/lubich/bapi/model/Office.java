348f01480067648647d86b29ea4957bd369be205
nvrstyle <nvrstyle@gmail.com>
2020-07-16 23:56:11 +0300

    Added Office entity

---
+++ src/main/java/ru/lubich/bapi/model/Office.java
@@ -0,0 +1,142 @@
+package ru.lubich.bapi.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Version;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Офис
+ */
+@Entity
+@Table(name = "Office")
+public class Office {
+
+    /**
+     * Идентификатор
+     */
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    /**
+     * Служебное поле hibernate
+     */
+    @Version
+    private Integer version;
+
+    /**
+     * Название офиса
+     */
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    /**
+     * Адрес офиса
+     */
+    @Column(name = "address", length = 200, nullable = false)
+    private String address;
+
+    /**
+     * Номер телефона офиса
+     */
+    @Column(name = "phone", length = 20)
+    private String phone;
+
+    /**
+     * Действующий ли офис
+     */
+    @Column(name = "is_active")
+    private Boolean isActive = false;
+
+    /**
+     * Организация. к которой относится офис
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "org_id", nullable = false)
+    private Organization organization;
+
+    /**
+     * Список работников офиса
+     */
+    @OneToMany(mappedBy = "office", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<User> users = new ArrayList<>();;
+
+    /**
+     * Конструктор для hibernate
+     */
+    public Office() {
+
+    }
+
+    /**
+     * Конструктор
+     *
+     * @param name         название офиса
+     * @param address      адрес офиса
+     * @param phone        номер телефона
+     * @param isActive     действует ли офис
+     * @param organization организация, к которой относится офис
+     */
+    public Office(String name, String address, String phone, Boolean isActive, Organization organization) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.isActive = isActive;
+        this.organization = organization;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
+}
