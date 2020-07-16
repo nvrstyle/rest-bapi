eb6c01e124e9ad7bf2784d9e090911a29f700a76
nvrstyle <nvrstyle@gmail.com>
2020-07-16 20:57:26 +0300

    Added DTO for DocType entity

---
+++ src/main/java/ru/lubich/bapi/view/DocTypeView.java
@@ -0,0 +1,25 @@
+package ru.lubich.bapi.view;
+
+/**
+ * Класс представления вилов документов, удостоверяющих личность физического лица
+ */
+public class DocTypeView {
+
+    /**
+     * Название документа
+     */
+    public String name;
+
+    /**
+     * Код документа
+     */
+    public String code;
+
+    @Override
+    public String toString() {
+        return "DocTypeView{" +
+                "name='" + name + '\'' +
+                ", code='" + code + '\'' +
+                '}';
+    }
+}
