30823764a554e6bcf2ee8a2197daaf4525dbc6c4
nvrstyle <nvrstyle@gmail.com>
2020-07-22 22:56:57 +0300

    Added custom exception

---
+++ src/main/java/ru/lubich/bapi/exception/InnerException.java
@@ -0,0 +1,17 @@
+package ru.lubich.bapi.exception;
+
+/**
+ * Исключение, возникающее в ходе работы приложения
+ */
+public class InnerException extends RuntimeException {
+
+    /**
+     * Конструктор
+     *
+     * @param message сообщение, описывающее исключительную ситуацию
+     * @param exception исключение, которое произошло во время работы приложения
+     */
+    public InnerException(String message, Throwable exception) {
+        super(message, exception);
+    }
+}
