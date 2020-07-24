cb4ecca1de722ebaffff1674267f2f9216f835c6
nvrstyle <nvrstyle@gmail.com>
2020-07-24 13:12:12 +0300

    Added DTO for organization entity

---
+++ src/main/java/ru/lubich/bapi/view/OrganizationSaveView.java
@@ -0,0 +1,55 @@
+package ru.lubich.bapi.view;
+
+/**
+ * Класс для сохранения входных данных новой организации
+ */
+public class OrganizationSaveView {
+
+    /**
+     * Название организации
+     */
+    public String name;
+
+    /**
+     * Полное название организации
+     */
+    public String fullName;
+
+    /**
+     * ИНН организации
+     */
+    public String inn;
+
+    /**
+     * КПП организации
+     */
+    public String kpp;
+
+    /**
+     * Адрес местонахождения организации
+     */
+    public String address;
+
+    /**
+     * Телефон организации
+     */
+    public String phone;
+
+    /**
+     * Флаг, показывающий является ли организация действующей
+     */
+    public Boolean isActive;
+
+    @Override
+    public String toString() {
+        return "OrganizationToSave{" +
+                "name='" + name + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", inn='" + inn + '\'' +
+                ", kpp='" + kpp + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
---
+++ src/main/java/ru/lubich/bapi/view/OrganizationView.java
@@ -0,0 +1,61 @@
+package ru.lubich.bapi.view;
+
+/**
+ * Класс представления организации
+ */
+public class OrganizationView {
+
+    /**
+     * Идентификатор организации
+     */
+    public Long id;
+
+    /**
+     * Название организации
+     */
+    public String name;
+
+    /**
+     * Полное название организации
+     */
+    public String fullName;
+
+    /**
+     * ИНН организации
+     */
+    public String inn;
+
+    /**
+     * КПП организации
+     */
+    public String kpp;
+
+    /**
+     * Адрес местонахождения организации
+     */
+    public String address;
+
+    /**
+     * Телефон организации
+     */
+    public String phone;
+
+    /**
+     * Флаг, показывающий является ли организация действующей
+     */
+    public Boolean isActive;
+
+    @Override
+    public String toString() {
+        return "OrganizationView{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", inn='" + inn + '\'' +
+                ", kpp='" + kpp + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
