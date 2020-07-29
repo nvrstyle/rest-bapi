bbb8206f3049ad7eec199599cc9eea41fdac3531
nvrstyle <nvrstyle@gmail.com>
2020-07-29 19:23:53 +0300

    Refactored logic of exception handling

--- src/main/java/ru/lubich/bapi/controller/response/ExceptionResponse.java
+++ src/main/java/ru/lubich/bapi/controller/response/ExceptionResponse.java
@@ -2,12 +2,18 @@ package ru.lubich.bapi.controller.response;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.lubich.bapi.exception.InnerException;
 import ru.lubich.bapi.view.ErrorView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestControllerAdvice
@@ -16,14 +22,19 @@ public class ExceptionResponse {
     @ExceptionHandler({InnerException.class, HttpMessageNotReadableException.class, HttpRequestMethodNotSupportedException.class})
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ErrorView exceptionHandler(RuntimeException e) {
-        ErrorView errorView = new ErrorView(e.getLocalizedMessage());
+        ErrorView errorView = new ErrorView(e.getMessage());
         return errorView;
     }
 
-    @ExceptionHandler({Exception.class})
+    @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorView unexpectedExceptionHandler(Exception e) {
-        ErrorView errorView = new ErrorView(e.getLocalizedMessage());
+    public ErrorView unexpectedExceptionHandler(MethodArgumentNotValidException e) {
+        InnerException innerException = new InnerException("Не задан обязательный параметр", e);
+        final List<String> errors = new ArrayList<String>();
+        for (final FieldError error : e.getBindingResult().getFieldErrors()) {
+            errors.add(error.getDefaultMessage());
+        }
+        ErrorView errorView = new ErrorView(innerException.getMessage() + ": " + errors);
         return errorView;
     }
 }
